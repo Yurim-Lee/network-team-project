faff688e1e4a442d417894261f00c660b3347eb8
Yurim-Lee <44902157+Yurim-Lee@users.noreply.github.com>
2022-11-28 16:21:44 +0900

    Create dataset_shuffle.py

---
+++ code/dataset_shuffle.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+#dataset import
+df=pd.read_csv('./(block) ton_memory.csv')
+
+#shuffle
+df = df.sample(frac=1)
+df
+
+#check cutoff (7:3 비율로 학습:검증 데이터 셋 나누기 위한 부분)
+df.iloc[7000:7001,:1]
+cutoff = 1554234730
+
+#타임스탬프를 인덱스로 넣은 다음, 인덱스를 리셋 해주어서 최종적으로 시계열 정보를 모두 없앰
+df = df.set_index('ts')
+df
+df.reset_index(drop=False, inplace=True)
+df
+
+#shuffle 데이터 셋을 csv 파일로 저장
+df.to_csv("(block) shuffle dataset.csv")
